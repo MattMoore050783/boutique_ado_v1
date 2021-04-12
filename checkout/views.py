7830f7060ab77ca367f1eb7d607494723a9f0efc
Matt Moore <58918583+MattMoore050783@users.noreply.github.com>
2021-04-12 13:49:23 +0100

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IfOWqFQkej05g5ieipHvcBE6zrFakpeJlsfdN1jOuurGQTSby605M0oF1YT906RzM4nPIKn1bCafZMZ2sI4A0n700DJfbywJw',
+        'client_secret': 'sk_test_51IfOWqFQkej05g5irrxgxWf9T2lfvguB9Xr2dgqqwLxJLBSzfnz21z9a2fhugnF9lc8TwhTadv7GoB7gtDoOuq3a00vqW1X40n',
     }
 
     return render(request, template, context)
